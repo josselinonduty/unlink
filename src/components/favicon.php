66e833728a044e1d0ff88489519289c987e93662
josselinonduty <contact@josselinonduty.fr>
2024-11-04 02:05:50 +0100

    feat(assets): create favicon

---
+++ src/components/favicon.php
@@ -0,0 +1,6 @@
+    <link rel="icon" type="image/png" href="/public/icon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/public/icon/favicon.svg" />
+    <link rel="shortcut icon" href="/public/icon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/icon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Unlink" />
+    <link rel="manifest" href="/public/icon/site.webmanifest" />
